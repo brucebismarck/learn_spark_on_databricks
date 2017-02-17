2a3fa3e2e8cea259b7e91dbfd120297e3f7d1c44
brucebismarck <brucebismarck@users.noreply.github.com>
2017-02-16 21:19:57 -0500

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -72,7 +72,8 @@ for item in string_col:
 df_price = df.join(df_str,'Id','inner').select('Id','SalePrice')
 df_variable = df.drop('SalePrice')
 
-data = df.rdd.map(lambda r: LabeledPoint(r[-1],                                  [r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9],r[10],r[11],r[12],r[13],r[14],r[15],r[16],r[17],r[18],r[19],r[20],r[21],r[22],r[23],r[24],r[25],r[26],r[27],r[28],r[29],r[30],r[31],r[32],r[33],r[34],r[35],r[36],r[37],r[38]])).toDF()                                                         # I don't know how to make it here. just use stupid method...
+data = df.rdd.map(lambda r: LabeledPoint(r[-1],[r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9],r[10],r[11],r[12],r[13],r[14],r[15],r[16],r[17],r[18],r[19],r[20],r[21],r[22],r[23],r[24],r[25],r[26],r[27],r[28],r[29],r[30],r[31],r[32],r[33],r[34],r[35],r[36],r[37],r[38]])).toDF()                                                        
+# I don't know how to make it here. just use stupid method...
 
 as_ml = udf(lambda v: v.asML() if v is not None else None, VectorUDT())          # define function to change datatype input vector.
 result = data.withColumn("features", as_ml("features"))
